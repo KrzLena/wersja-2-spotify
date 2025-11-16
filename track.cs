360b42120b28f409839d37404bb8ff3bb2766bf3
Kciuk <135543040+KrzLena@users.noreply.github.com>
2025-11-16 22:27:14 +0100

    1

---
+++ track.cs
@@ -0,0 +1,9 @@
+namespace OdtwarzaczMuzyki.Models
+{
+    public class Track
+    {
+        public string Title { get; set; }
+        public string FilePath { get; set; }
+        public string ArtworkPath { get; set; }
+    }
+}
